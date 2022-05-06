f50e43bf45d2a3d1436f607ca3042c6796e4be35
Arvine Gopez <gopezarvine@ymail.com>
2022-05-06 09:43:32 -0500

    Added a port 2906

--- server/app.js
+++ server/app.js
@@ -1,1 +1,13 @@
 const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+    graphiql: true
+}))
+
+
+app.listen(2906, () => {
+    console.log('Listening for request on my Port 2906');
+})
