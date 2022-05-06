ae3239b49c7efa46f5d1aa3ed090c6c6a41ecfbb
Arvine Gopez <gopezarvine@ymail.com>
2022-05-06 10:16:52 -0500

    Testing on some of the code

--- server/schema/scheme.js
+++ server/schema/scheme.js
@@ -3,10 +3,12 @@ const graphql = require('graphql');
 
 const {
     GraphQLObjectType,
+    GraphQLSchema,
     GraphQLID,
     GraphQLString,
     GraphQLInt,
     
+
 
 
 } = graphql
@@ -16,9 +18,33 @@ const UserType = new graphql.GraphQLObjectType({
     name: 'User',
     description: 'Documentation for User...',
     fields: () => ({
-        id: {type: GraphQLID},
+        id: {type: GraphQLString},
         name: {type: GraphQLString},
         age: {type: GraphQLInt}
     })
-
 });
+
+// Root Query 
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    description: 'Description',
+    fields: {
+        user: {
+            type: UserType,
+            args: {id: {type: GraphQLString}},
+            resolve(parent, args){
+            
+                // Resolve with Data
+                // Get and Return data from
+                // Data Source
+              
+
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+})
